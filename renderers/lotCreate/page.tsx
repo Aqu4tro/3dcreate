81127e243ff05bb33b6f934fe28703bd043d47e1
Jonathas <jonathaspolos7@gmail.com>
2025-01-06 20:22:23 -0300

    feat: Adicionando angulo do telhado.

--- renderers/lotCreate/page.tsx
+++ renderers/lotCreate/page.tsx
@@ -2,6 +2,7 @@ import { Room } from "@/app/page";
 import * as THREE from "three";
 import Block from "../blockCreate/page";
 import { ThreeEvent } from "@react-three/fiber";
+import { Plane } from "@react-three/drei";
 
 
 interface LotProps extends Room {
@@ -13,10 +14,10 @@ export function walls({ width, length, tickLot, topSize, size, height, angle_Top
   const walls = [];
   const sizePin = size / 2;
 
-  walls.push({ x: 0, y: (height / 2 - (topSize - tickLot) / 2) + angle_Top.b * 1.6, z: -length / 2 + sizePin, H: height - (topSize + tickLot), W: width, L: size });
-  walls.push({ x: 0, y: (height / 2 - (topSize - tickLot) / 2) + angle_Top.f, z: length / 2 - sizePin, H: height - (topSize + tickLot), W: width, L: size });
-  walls.push({ x: width / 2 - sizePin, y: (height / 2 - (topSize - tickLot) / 2) + angle_Top.r, z: 0, H: height - (topSize + tickLot), W: size, L: length });
-  walls.push({ x: -width / 2 + sizePin, y: (height / 2 - (topSize - tickLot) / 2) + angle_Top.l, z: 0, H: height - (topSize + tickLot), W: size, L: length });
+  walls.push({ x: 0, y: (height / 2 - (topSize - tickLot) / 2) + angle_Top.f * 4, z: -length / 2 + sizePin, H: height - (topSize + tickLot) + angle_Top.f * 8 , W: width, L: size , N: "F"});
+  walls.push({ x: 0, y: (height / 2 - (topSize - tickLot) / 2) + angle_Top.b * 4, z: length / 2 - sizePin, H: height - (topSize + tickLot)  + (angle_Top.b) * 8 , W: width, L: size, N: "B"});
+  walls.push({ x: width / 2 - sizePin, y: (height / 2 - (topSize - tickLot) / 2) + angle_Top.r, z: 0, H: height - (topSize + tickLot), W: size, L: length , N:"R"});
+  walls.push({ x: -width / 2 + sizePin, y: (height / 2 - (topSize - tickLot) / 2) + angle_Top.l, z: 0, H: height - (topSize + tickLot), W: size, L: length, N:"L" });
 
   return walls;
 }
@@ -39,6 +40,7 @@ export default function Lot({
   rotation,
   angle_Top,
 }: LotProps) {
+
 
   // Function to toggle selection state
   function switchSelect(event: ThreeEvent<MouseEvent> | MouseEvent | TouchEvent) {
@@ -66,21 +68,66 @@ export default function Lot({
 
             {/* Render top surface if height is defined */}
             {height && top && (
-              <mesh position={[0, height - .05 + (angle_Top.b / 1.6) ,0]} rotation={new THREE.Euler(angle_Top.b / 4 || -angle_Top.f,0,angle_Top.r)} name="top-surface"> 
+              <mesh position={[0, height - .05 + (angle_Top.b || angle_Top.f ? angle_Top.f + 0.15 * 2 : 0) ,0]} rotation={new THREE.Euler(angle_Top.b / 4 || -angle_Top.f,0,angle_Top.r)} name="top-surface"> 
             <boxGeometry args={[width, .1, length]} /> 
                 <meshBasicMaterial color={"blue"} />
               </mesh>
             )}
 
             {/* Render walls if dimensions are defined */}
-            {height && size && tickLot && walls({ width, height, size, length, tickLot, topSize: .1, angle_Top }).map((e, index) => (
-              <mesh key={index} position={[e.x,e.y,e.z]} rotation={new THREE.Euler(0,0,0)} name={`wall-${index}`}> 
+            {height && size && tickLot && walls({ width, height, size, length, tickLot, topSize: .1, angle_Top }).map((e, index) => {
+                let vertices = new Float32Array();
+                switch (e.N) {
+                case "R":
+                    vertices = new Float32Array(angle_Top.b ? [width, height + angle_Top.f * 8,-length, width, height, length, width, height, -length] : (angle_Top.f ? [1, 2, 3] : []));
+                    break;
+                  case "L":
+                    vertices = new Float32Array(angle_Top.b ? [1, 2, 3] : (angle_Top.f ? [1, 2, 3] : []));
+                    break;
+                  case "F":
+                    vertices = new Float32Array(angle_Top.r ? [1, 2, 3] : (angle_Top.l ? [1, 2, 3] : []));
+                    break;
+                  case "B":
+                    vertices = new Float32Array(angle_Top.r ? [1, 2, 3] : (angle_Top.l ? [1, 2, 3] : []));
+                  break;
+               
+                }
+
+                const indices = new Uint16Array([
+                0, 1, 2,
+                // Add more indices as needed
+                ]);
+
+                return (
+                <mesh key={index} position={[e.x, e.y, e.z]} rotation={new THREE.Euler(0, 0, 0)} name={`wall-${index}`}>
+                  <mesh>
+                  
+                    
+                    <bufferGeometry>
+                      <bufferAttribute
+                      attach="attributes-position"
+                      array={vertices}
+                      count={vertices.length / 3}
+                      itemSize={3}
+                      />
+                      <bufferAttribute
+                      attach="index"
+                      array={indices}
+                      count={indices.length}
+                      itemSize={1}
+                      />
+                    </bufferGeometry>
+                   
+                  </mesh>
+                
+                
                 <boxGeometry args={[e.W, e.H, e.L]} />
-                
-                
+               
                 <meshBasicMaterial color={"pink"} />
               </mesh>
-            ))}
+               
+               
+)})}
 
             {/* Render additional objects */}
             {objects?.map((e) => (
